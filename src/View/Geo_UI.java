50bd2990aa508ad522c3aa490de7dca8af1204cb
Matt Peterson <matt.peterson.3519@gmail.com>
2017-05-25 15:14:41 -0500

    Changed Comment, added 2

--- src/View/Geo_UI.java
+++ src/View/Geo_UI.java
@@ -22,7 +22,7 @@ public class Geo_UI extends JFrame{
 	//top Panel and child button declaration
 	private JPanel topPanel;
 	private JLabel introLabel;
-	//hey this is a random comment
+	//hey this is a random comment 2
 	private JButton connectChildren, reset;
 	
 	//Canvas panel
